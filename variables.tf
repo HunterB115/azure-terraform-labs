1017f893354308119527c04eb5559f6645a331b7
HunterB115 <hunt9823@gmail.com>
2026-03-04 20:44:06 -0500

    created VNETs and subnets for each of the RGs

--- variables.tf
+++ variables.tf
@@ -1,21 +1,34 @@
-variable resource_groups {
-    type = map(string)
-    description = "The resource groups to deploy"
+variable "resource_groups" {
+  type        = map(string)
+  description = "The resource groups to deploy"
 }
 
 variable "prefix" {}
 
 variable "region" {
-    type = string
-    default = "East US"
-    description = "The Azure region to deploy resources"
-    validation {
-        condition = contains(["UK South", "UK West", "North Europe", "West Europe", "East US", "West US"], var.region)
-        error_message = "Invalid Region"
-    }
+  type        = string
+  default     = "East US"
+  description = "The Azure region to deploy resources"
+  validation {
+    condition     = contains(["UK South", "UK West", "North Europe", "West Europe", "East US", "West US"], var.region)
+    error_message = "Invalid Region"
+  }
 }
 
 variable "tags" {
-    type = map(any)
-    description = "a map of tags"
+  type        = map(any)
+  description = "a map of tags"
+}
+
+variable "virtual_networks" {
+  type = map(object({
+    name               = string
+    resource_group_key = string
+    address_space      = list(string)
+    subnets = map(object({
+      name           = optional(string)
+      address_prefix = string
+    }))
+  }))
+  description = "The virtual networks to deploy"
 }
